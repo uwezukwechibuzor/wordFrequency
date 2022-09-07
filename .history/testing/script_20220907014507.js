a137c2036d72969b40d71724c23db6ad0672a348
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2022-09-07 01:52:33 +0100

    remove file

--- .history/testing/script_20220907014507.js
+++
@@ -1,5 +0,0 @@
-
-//testing for the function before publishing to npm
-
-const wordFrequency = require("@chibuzor/word-freq-counter");
-console.log(wordFrequency("I am who I am"));
--- .history/testing/script_20220907014910.js
+++
@@ -1,5 +0,0 @@
-
-//testing for the function before publishing to npm
-
-const wordFrequency = require("word-freq-counter");
-console.log(wordFrequency("I am who I am"));
